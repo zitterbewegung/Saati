fae18347e397e46e5a451e52ca0167536286eacd
r2q2 <r2q2@balthazar>
2021-06-20 11:45:06 -0500

    Webchat is now set to production mode.

--- src/webchat.py
+++ src/webchat.py
@@ -1,73 +1,79 @@
 from flask import Flask, render_template, request, session, make_response
-#from chatterbot import ChatBot
-#from chatterbot.trainers import ChatterBotCorpusTrainer
+
+# from chatterbot import ChatBot
+# from chatterbot.trainers import ChatterBotCorpusTrainer
 from flask import send_file
 
 from logic import answer_question
-import sys, logging, uuid
+import sys, logging, os, uuid
 
-app = Flask('saati')
+app = Flask("saati")
+SECRET_KEY = os.environ["SECRET_KEY"]
 
-
-#create chatbot
-#englishBot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
-#trainer = ChatterBotCorpusTrainer(englishBot)
-#trainer.train("chatterbot.corpus.english") #train the chatter bot for english
+# create chatbot
+# englishBot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+# trainer = ChatterBotCorpusTrainer(englishBot)
+# trainer.train("chatterbot.corpus.english") #train the chatter bot for english
 
 handler = logging.StreamHandler(sys.stdout)
-handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+handler.setFormatter(
+    logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+)
 app.logger.addHandler(handler)
 app.logger.setLevel(logging.DEBUG)
 
-#define app routes
+# define app routes
 @app.route("/")
 def index():
-    #user_identifier = session.get('identifier', uuid.uuid4())
-    #resp.set_cookie('userID', session['identifier'])
+    # user_identifier = session.get('identifier', uuid.uuid4())
+    # resp.set_cookie('userID', session['identifier'])
     return render_template("chatbot.html")
+
 
 @app.route("/talk")
 def talk():
-    #user_identifier = session.get('identifier', uuid.uuid4())
-    #resp.set_cookie('userID', session['identifier'])
+    # user_identifier = session.get('identifier', uuid.uuid4())
+    # resp.set_cookie('userID', session['identifier'])
     return render_template("talk.html")
 
 
-#@app.route('/setcookie', methods = ['POST', 'GET'])
-#def setcookie():
+# @app.route('/setcookie', methods = ['POST', 'GET'])
+# def setcookie():
 #   if request.method == 'POST':
 #    user = request.form['nm']
-   
+
 #   resp = make_response(render_template('readcookie.html'))
 #   resp.set_cookie('userID', user)
-   
+
 #   return resp
 
 
-@app.route('/get_image')
+@app.route("/get_image")
 def get_image():
-    if request.args.get('type') == '1':
-       filename = 'ok.gif'
+    if request.args.get("type") == "1":
+        filename = "ok.gif"
     else:
-       filename = 'error.jpg'
-    return send_file(filename, mimetype='image/jpg')
+        filename = "error.jpg"
+    return send_file(filename, mimetype="image/jpg")
+
 
 @app.route("/get")
-#function for the bot response
+# function for the bot response
 def get_bot_response():
-    #resp = make_response()
-    #if not request.cookies.get('userID'):
-    #   
-    
-    cookieid = request.cookies.get('userID')
-    #user_identifier = session.get('identifier', uuid.uuid4())
+    # resp = make_response()
+    # if not request.cookies.get('userID'):
+    #
 
-    userText = request.args.get('msg')
+    cookieid = request.cookies.get("userID")
+    # user_identifier = session.get('identifier', uuid.uuid4())
+
+    userText = request.args.get("msg")
     user_identifier = str(request.remote_addr)
-    inference = answer_question(userText, user_identifier, 'webchat')
+    inference = answer_question(userText, user_identifier, "webchat")
     return inference
 
-    #return str(englishBot.get_response(userText))
+    # return str(englishBot.get_response(userText))
+
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(host="0.0.0.0", debug=False, port=5002)
